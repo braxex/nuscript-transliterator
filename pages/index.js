e8f297ca3d0e75aab28838e6139ac7c802fdcc1b
Braxton Excell <braxton.excell@gmail.com>
2021-10-24 16:17:22 -0500

    linting fixes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 // components
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-
-import { getRedisEntry, setRedisEntry } from '../src/utils/redis-utils'
 
 export default function Homepage() {
   return (
